2ee5e4879848647eebcd8ceb53dd6fcf78395517
Dustin Oprea <myselfasunder@gmail.com>
2015-05-11 03:44:39 -0400

    Added packaging. Added ignores.

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+import os
+
+import fss
+
+APP_PATH = os.path.dirname(fss.__file__)
+
+with open(os.path.join(APP_PATH, 'resources', 'README.rst')) as f:
+      _LONG_DESCRIPTION = f.read()
+
+with open(os.path.join(APP_PATH, 'resources', 'requirements.txt')) as f:
+      _INSTALL_REQUIRES = list(map(lambda s: s.strip(), f.readlines()))
+
+_DESCRIPTION = \
+    "Search a filesystem using zero or more file and directory filters."
+
+setuptools.setup(
+    name='fsscanner',
+    version=fss.__version__,
+    description=_DESCRIPTION,
+    long_description=_LONG_DESCRIPTION,
+    classifiers=[
+    ],
+    keywords='filesystem file search scanner',
+    author='Dustin Oprea',
+    author_email='myselfasunder@gmail.com',
+#    url='https://github.com/dsoprea/PyInotify',
+    license='GPL 2',
+    packages=setuptools.find_packages(exclude=['dev']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=_INSTALL_REQUIRES,
+    package_data={
+        'fss': [
+            'resources/README.rst',
+            'resources/requirements.txt',
+        ]
+    }
+)
